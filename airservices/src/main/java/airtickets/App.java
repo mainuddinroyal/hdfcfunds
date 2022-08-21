5c994434289645cd781d838c61b06e7bf2dbf113
@gouse104v <gouse104v@gmail.com>
2022-08-21 19:30:40 +0530

    push by gouse

---
+++ airservices/src/main/java/airtickets/App.java
@@ -0,0 +1,13 @@
+package airtickets;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
